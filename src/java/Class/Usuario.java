ea65276ec91c62431b4c011316f921497105a595
Detonador <Detonador@Detonador-PC>
2015-08-05 19:32:15 -0300

    start do projeto

--- src/java/Class/Usuario.java
+++ src/java/Class/Usuario.java
@@ -10,5 +10,51 @@ package Class;
  * @author Detonador
  */
 public class Usuario {
-    
+  private int nivel;
+  private String nome;
+  private String email;
+  private int matricula;
+  private int CPF;
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(int CPF) {
+        this.CPF = CPF;
+    }
+  
+  
 }
